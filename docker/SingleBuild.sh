d9a071b2a98e7484cd4f17d42e7caece7eeb11b0
Adithya Yerramsetty <adithyay@hotmail.com>
2023-07-04 14:50:52 -0700

    Also added docker pull to SingleBuild, which pulls the latest container.

--- docker/SingleBuild.sh
+++ docker/SingleBuild.sh
@@ -2,5 +2,5 @@
 # instead of starting the autorun
 # from the DockerRun.sh
 # script.
-
+docker pull adithyay328/sphinx_site_template
 docker run --mount type=bind,source="$(cd ../ && pwd)",target=/app adithyay328/sphinx_site_template bash -c 'cd /app/site; sphinx-build -b html source build'
