0467780d8a1c754111bae689ae5d9de2382a70f2
Adithya Yerramsetty <adithyay@hotmail.com>
2023-07-21 20:14:54 -0700

    Builds work on my laptop! Testing on my machine, but this code is amazing now. The task graph approach works quite nicely.

--- build/runner.py
+++ build/runner.py
@@ -2,13 +2,13 @@
 # build process
 from datetime import datetime, timezone, timedelta
 import time
+import json
 
-import networkx as nx
-
-import encrypt
 import buildConfig
 import githubAPI
 import taskgraph
+
+BRANCH_HASHES_FNAME = "branchHashes.json"
 
 if __name__ == "__main__":
   verified_key = ""
@@ -38,30 +38,58 @@ if __name__ == "__main__":
   # between builds
   secondsBetweenBuilds = buildConfigObj.secondsBetweenBuilds
 
-  print(f"Waiting {secondsBetweenBuilds} seconds between builds...")
-  
-  # Testing: Creating a branch object, and adding it to the task
-    # graph
-  branch = taskgraph.Branch("develop", "orphaned", "removed")
-  taskgraph.taskGraph.addItem(branch)
-
-  print(taskgraph.taskGraph._graph.nodes())
-  print(taskgraph.taskGraph._graph.edges())
-
-  taskgraph.taskGraph.fixItems()
-
   # While loop for builds:
   while True:
-    break
     # Get start time, and compute earliest end time
     startTime = datetime.now(timezone.utc)
     endTime = startTime + timedelta(seconds=secondsBetweenBuilds)
 
+    print(f"Waiting {secondsBetweenBuilds} seconds between builds...")
+
     # Get the remote branches and SHA hashes
     remoteBranchesAndSHA = githubHandle.getRemoteBranchesAndSHA()
-    print(remoteBranchesAndSHA)
+    
+    # Load the most recent branch hashes
+    mostRecentBranchHashes = {}
+    with open(BRANCH_HASHES_FNAME) as f:
+      mostRecentBranchHashes.update(json.loads(f.read()))
+    
+    # Now, compare the remote branches and SHA hashes;
+    # if any are different, change state to out_of_date.
+    # If any are orphaned, update state to orphaned
+    # and change target state to removed
+    # If any new branches are found, add them to the
+    # task graph with state uncloned
+    for branchName, sha in remoteBranchesAndSHA.items():
+      if branchName not in mostRecentBranchHashes:
+        # Add the branch to the task graph
+        newBranch = taskgraph.Branch(branchName, "uncloned", "deployed")
+        taskgraph.taskGraph.addItem(newBranch)
+      elif sha != mostRecentBranchHashes[branchName]:
+        # Update the branch state to out_of_date
+        taskgraph.taskGraph.updateItemStates(branchName, "out_of_date", "deployed")
+    
+    # Now, check for orphaned branches
+    for branchName in mostRecentBranchHashes.keys():
+      if branchName not in remoteBranchesAndSHA:
+        # Update the branch state to orphaned
+        taskgraph.taskGraph.updateItemStates(branchName, "orphaned", "removed")
+      
+    # Now, run the task graph
+    taskgraph.taskGraph.fixItems()
+
+    # Now, delete all branch objects that are
+    # in the removed state
+    for branchName in mostRecentBranchHashes.keys():
+      if branchName not in remoteBranchesAndSHA:
+        # Delete the branch object
+        taskgraph.taskGraph.removeItem(branchName)
+    
+    # Now, save the most recent branch hashes
+    with open(BRANCH_HASHES_FNAME, "w") as f:
+      f.write(json.dumps(remoteBranchesAndSHA))
 
     # # Ensure we have waited long enough
-    # currTime = datetime.now(timezone.utc)
-    # if currTime < endTime:
-    #   time.sleep((endTime - currTime).total_seconds())
+    currTime = datetime.now(timezone.utc)
+    if currTime < endTime:
+      time.sleep((endTime - currTime).total_seconds())
